07733ba9cc388892c862b1054d3159740fac56a4
Ashwin Pottekkat <58938307+ashwin142000@users.noreply.github.com>
2024-04-11 23:42:15 +0530

    Add files via upload

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+const initialBoard =[
+[null,null,null],
+[null,null,null],
+[null,null,null],
+];
+export default function GameBoard({onSelectSquare,turns}){
+    let gameBoard = initialBoard;
+
+    for (const turn of turns) {
+        const { square, player } = turn;
+        const { row, col } = square;
+
+        gameBoard[row][col] = player;
+  }
+    // const [gameBoard,setGameBoard]=useState(initialBoard);
+    
+    // function handleSelectSquare(rowIndex,colIndex){
+    //     setGameBoard((prevGameBoard)=>{
+    //         const updatedBoard=[...prevGameBoard.map(innerArray=>[...innerArray])];
+    //         updatedBoard[rowIndex][colIndex]=activePlayerSymbol;
+    //         return updatedBoard;
+    //     });
+    //     onSelectSquare();
+    // }
+    return (
+        <ol id="game-board">
+        {gameBoard.map((row,rowIndex)=> 
+        <li key={rowIndex}>
+            <ol>
+                {row.map((col,colIndex)=> 
+                <li key={colIndex}>
+                    <button onClick={()=>onSelectSquare(rowIndex,colIndex)}>{col}</button>
+                </li>)}
+            </ol>
+        </li>)}
+        </ol>
+    );
+}
